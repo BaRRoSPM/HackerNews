f9acbd349cda9802b9b9c56d13b58bb0ec78c65b
Pedro Barros <pedro.barros@gfi.pt>
2020-07-26 16:02:43 +0100

    Add project files.

---
+++ Api/Controllers/HackerNewsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HackerNewsController : ControllerBase
+    {
+        private readonly ILogger<HackerNewsController> _logger;
+        private readonly IConfiguration _config;
+        private IMemoryCache _cache;
+
+        public HackerNewsController(ILogger<HackerNewsController> logger, IMemoryCache memoryCache, IConfiguration configuration)
+        {
+            _logger = logger;
+            _config = configuration;
+            _cache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<HackerNewsStory>> Get()
+        {
+            const string cacheKey = "HackerNewsResult";
+
+            _logger.LogInformation("HackerNews-Get: Called");
+
+            // Get Cache slide and absolute expire values from settings file, if not exists use hardcoded default values
+            int slideExpireSecs = _config.GetValue<int?>("Cache:SlidingExpiration") ?? 30;
+            int absExpireSecs = _config.GetValue<int?>("Cache:AbsoluteExpiration") ?? 300;
+
+            IEnumerable<HackerNewsStory> result;
+            try
+            {
+                // Check if item is already in MemCache, 
+                // if it is then return the cached version, if not call the hackernews webservice and place it on cache
+                if (!_cache.TryGetValue(cacheKey, out result))
+                {
+                    _logger.LogDebug("HackerNews-Get: Getting from external webservice");
+
+                    // Call base API
+                    result = await HackerNewsClient.GetHackerNewsStories(20);
+
+                    // Add returned item to cache using the already defined expiration values
+                    _cache.Set(cacheKey,
+                               result,
+                               new MemoryCacheEntryOptions()
+                               {
+                                   SlidingExpiration = TimeSpan.FromSeconds(slideExpireSecs),
+                                   AbsoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(absExpireSecs)
+                               });
+                }
+                else
+                {
+                    _logger.LogDebug("HackerNews-Get: Getting from cache");
+                }
+
+                _logger.LogInformation("HackerNews-Get: Returning json item");
+                return result.OrderByDescending(x => x.score);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurd while calling the HackerNews API.");
+                throw;
+            }
+        }
+    }
+}
---
+++ Api/HackerNewsClient.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public static class HackerNewsClient
+    {
+        private const string baseEndpoint = "https://hacker-news.firebaseio.com/v0"; // this should be placed in a config file.
+
+        public async static Task<IEnumerable<HackerNewsStory>> GetHackerNewsStories(int numberOfStories = 0)
+        {
+            var result = new List<HackerNewsStory>();
+
+            var stories = await getBestStories();
+
+            if (numberOfStories > 0) stories = stories.Take(numberOfStories);
+
+            foreach (var s in stories)
+            {
+                var d = await getStoryDetails(s);
+                var story = new HackerNewsStory()
+                {
+                    title = d.title,
+                    uri = d.url,
+                    postedBy = d.by,
+                    time = DateTimeOffset.FromUnixTimeSeconds(d.time).DateTime, // HackerNews API returns dates in UnixTime
+                    score = d.score,
+                    commentCount = d.descendants
+                };
+                result.Add(story);
+            }
+            return result;
+        }
+
+        private async static Task<IEnumerable<int>> getBestStories()
+        {
+            string endpoint = $"{baseEndpoint}/beststories.json"; // this should be placed in a config file.
+            var result = await getFromEndpoint<IEnumerable<int>>(endpoint);
+            return result;
+        }
+
+        private async static Task<StoryDetail> getStoryDetails(int storyID)
+        {
+            string endpoint = $"{baseEndpoint}/item/{storyID}.json"; // this should be placed in a config file.
+            var result = await getFromEndpoint<StoryDetail>(endpoint);
+            return result;
+        }
+
+        /// <summary>
+        /// Generic method for calling a REST endpoint and returning a T result
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
+        private async static Task<T> getFromEndpoint<T>(string endpoint)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(endpoint);
+                var result = JsonConvert.DeserializeObject<T>(response);
+                return result;
+            }
+        }
+
+        // HackerNews Item details contract/proxy class
+        private class StoryDetail
+        {
+            public int id { get; set; }
+            public string deleted { get; set; }
+            public string type { get; set; }
+            public string by { get; set; }
+            public long time { get; set; }
+            public string text { get; set; }
+            public string dead { get; set; }
+            public int parent { get; set; }
+            public int pool { get; set; }
+            public IEnumerable<int> kids { get; set; }
+            public string url { get; set; }
+            public int score { get; set; }
+            public string title { get; set; }
+            public IEnumerable<int> parts { get; set; }
+            public int descendants { get; set; }
+        }
+    }
+}
